c2a2ab69bd1a0503e041765eaa84cf82c2c9eb76
Alexandre Gibson <alexandregibson@gmail.com>
2019-08-26 21:32:42 -0300

    Create new view controller for the products in the selected category and set its iboutlets.

---
+++ coder-swag/View/ProductCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Alexandre Gibson on 26/08/19.
+//  Copyright © 2019 GBSN. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
